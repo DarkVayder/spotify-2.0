b46809dc76de18f5ed8794cc5b1be7f8a2916492
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-09 19:13:33 +0100

    Build

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3002/"; 
+const redirectUri = "http://localhost:3000/"; 
 
 const clientId = "a82199fef3514db59d60ff4127b2a63b";
 
@@ -12,6 +12,5 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}
-&response_type=token&show_dialog=true`; 
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`; 
 
