bca2b7bd13a564df24896c5b87f4a0e6dd088d57
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-12 23:48:59 +0100

    feat

---
+++ src/DataLayer.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext, useReducer } from "react"; // Fix the import statement
+
+export const DataLayerContext = createContext();
+
+export const DataLayer = ({ initialState, reducer, children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState); // Fix useReducer arguments
+
+  return (
+    <DataLayerContext.Provider value={{ state, dispatch }}>
+      {children}
+    </DataLayerContext.Provider>
+  );
+};
